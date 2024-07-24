e02188affcfcd2960beed6e45043830ec1aecece
Vladimir Stanev <v.stanev39@gmail.com>
2024-07-24 14:46:16 +0300

    This is the initial commit

---
+++ tests/login_tests.spec.js
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+test('standart_user', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.getByText('Swag Labs')).toBeVisible();
+  await expect(page.locator('[data-test="inventory-list"]')).toBeVisible();
+});
+
+test('wrong_credentials', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill('Mariqn_Dobrev');
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('ObichamTrator');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service');
+  });
+  
+  test('locked_out_user', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill('locked_out_user');
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Sorry, this user has been locked out.');
+  });
+
+  test('test', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill('problem_user');
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.getByText('Swag Labs')).toBeVisible();
+    await expect(page.locator('[data-test="inventory-list"]')).toBeVisible();
+  });
---
+++ tests/produc_managing_and_order_placement.spec.js
@@ -0,0 +1,146 @@
+import { test, expect } from '@playwright/test';
+//test adding and removing items to cart from the catalogue page
+test('loading_and_unloading_cart', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await expect(page.locator('[data-test="shopping-cart-link"]')).toContainText('1');
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('2');
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('3');
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('2');
+  await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('1');
+  await page.locator('[data-test="remove-sauce-labs-bolt-t-shirt"]').click();
+});
+//checks if the catalogue page and product page are synced(if the items is added to cart or not)
+test('product_page_and_catalogue_sync', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('1');
+  await page.locator('[data-test="item-4-title-link"]').click();
+  await expect(page.locator('[data-test="remove"]')).toContainText('Remove');
+  await page.locator('[data-test="remove"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await expect(page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')).toContainText('Add to cart');
+});
+//checks if you can correctly see and remove items from cart in the my cart page
+test('correct_product_managing_from_cart', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await expect(page.getByText('1Sauce Labs Backpackcarry.')).toBeVisible();
+  await expect(page.getByText("1Sauce Labs Bike LightA red")).toBeVisible();
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+  await expect(page.getByText('1Sauce Labs Backpackcarry.')).not.toBeVisible();
+  await expect(page.getByText("1Sauce Labs Bike LightA red")).not.toBeVisible();
+});
+//tests the placement of an order
+test('normal_order', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('Marian');
+  await page.locator('[data-test="lastName"]').click();
+  await page.locator('[data-test="lastName"]').fill('Taratorest');
+  await page.locator('[data-test="postalCode"]').click();
+  await page.locator('[data-test="postalCode"]').fill('1111');
+  await page.locator('[data-test="continue"]').click();
+  await expect(page.locator('[data-test="cart-list"]')).toContainText('$29.99');
+  await expect(page.locator('[data-test="cart-list"]')).toContainText('$9.99');
+  await expect(page.locator('[data-test="subtotal-label"]')).toContainText('Item total: $39.98');
+  await page.locator('[data-test="finish"]').click();
+  await expect(page.locator('[data-test="complete-header"]')).toContainText('Thank you for your order!');
+});
+//tests the addint and removing items from the catalogue page in the problematic user
+test('unable_to_remove_cart_items_from_catalogue', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('problem_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('1');
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('1');
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('2');
+  await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('2');
+});
+//checks if the catalogue page and product page are synced(if the items is added to cart or not) problematic user case
+test('unsync_catalogue_and_product_page', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('problem_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toContainText('1');
+  await page.locator('[data-test="item-4-title-link"]').click();
+  await expect(page.locator('[data-test="add-to-cart"]')).toContainText('Add to cart');
+  await page.locator('[data-test="back-to-products"]').click();
+  await expect(page.locator('[data-test="remove-sauce-labs-backpack"]')).toContainText('Remove');
+});
+//tests if you can correctly add and remove items from the product page from the problematic user
+test('Unable_to_add_items_to_cart_from_product_page', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('problem_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="item-4-title-link"]').click();
+  await page.locator('[data-test="add-to-cart"]').click();
+  await page.locator('[data-test="secondary-header"]').click();
+  await expect(page.locator('[data-test="add-to-cart"]')).toContainText('Add to cart');
+});
+//tests the order placement of the problematic user
+test('unable_to_input_lastname', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('problem_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('miro');
+  await page.locator('[data-test="lastName"]').click();
+  await page.locator('[data-test="lastName"]').fill('Krasimirov');
+  await page.locator('[data-test="postalCode"]').click();
+  await page.locator('[data-test="postalCode"]').fill('1111');
+  await page.locator('[data-test="continue"]').click();
+  await expect(page.locator('[data-test="error"]')).toContainText('Error: Last Name is required');
+});
